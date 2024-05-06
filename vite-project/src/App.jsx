343daf0ea45e6a41cc60229d62eb1b77fbf715fe
grace ngure <wnguregrace@gmail.com>
2024-05-06 20:50:17 +0300

    feat:updated App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import NavBar from "./components/NavBar"
+import HomePage from "./components/HomePage"
+import PropertyListing from "./components/PropertyListing"
+import Testimonials from "./components/Testimonials "
+import Contacts from "./components/Contacts"
 
 function App() {
   return (
     <div>
-      
+      <NavBar />
+      <HomePage/>
+      <PropertyListing/>
+      <Testimonials/>
+      <Contacts/>
+
     </div>
   )
 }
